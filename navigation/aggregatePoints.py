06eb0d79e10a9a878ffaf594a521b5df3e693e4a
John Chiaramonte <chiaramonte.j@northeastern.edu>
2022-03-31 17:02:53 -0400

    Update aggregatePoints.py

--- navigation/aggregatePoints.py
+++ navigation/aggregatePoints.py
@@ -1,19 +1,20 @@
+from geometry_msgs.msg import *
 import rospy
-
 
 class AntiCluster:
     def __init__(self, x, y, init_node=False):
         if init_node:
             rospy.init_node('aggregate_points')
 
-        rospy.Subscriber("/trash_mapper/trash_points", Pose(), self.addPoint)
-        self._XLENGTH_ = x  # cm
-        self._YLENGTH_ = y  # cm
+        rospy.Subscriber("/trash_mapper/trash_points", Pose, self.addPoint)
+        self._XLENGTH_ = x  # m
+        self._YLENGTH_ = y  # m
         self.listOfPoints = []
 
     def addPoint(self, data):
         self.listOfPoints.append(data)
         self.listOfPoints = self.aggregatePoints(self.listOfPoints)
+	print('Points: %s' % self.listOfPoints)
 
     def aggregatePoints(self, points):
         x = 0
@@ -29,8 +30,8 @@ class AntiCluster:
 
             while y < endX:
                 if xRangeBottom <= points[y].position.x <= xRangeTop and yRangeBottom <= points[y].position.y <= yRangeTop:
-                    print(points[x])
-                    print(points[y])
+                    #print(points[x])
+                    #print(points[y])
                     points.pop(y)
                     endX = endX - 1
                 y = y + 1
@@ -41,9 +42,15 @@ class AntiCluster:
 
 
 if __name__ == "__main__":
+    '''
     first = [(1, 2), (20, 4), (3, 20), (20, 20), (1, 1), (21, 20), (40, 40)]
     print(first)
-
-    cluster = AntiCluster(2, 2)
+    
+    cluster = AntiCluster(0.2, 0.2, True)
     last = cluster.aggregatePoints(first)
     print(last)
+    '''
+    cluster = AntiCluster(0.2,0.2, True)
+
+    while True:
+        continue
